41ff3aeff0d761ba6d93222cc45798c45c8df92b
exit <phirom.kong@student.cadt.edu.kh>
2023-12-20 17:48:20 +0700

    Init

---
+++ src/data/constants.js
@@ -0,0 +1,129 @@
+export const Bio = {
+  name: "Kong Phirom",
+  roles: [
+    "Software Developer",
+    "Web Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a junior majoring in Computer Science specializing in Software Engineering at the Cambodia Academy of Digital Technology. I am an enthusiastic learner, always eager to explore new technologies and methodologies to improve my coding skills.",
+  github: "https://github.com/phiromkong",
+  resume:
+    "https://drive.google.com/file/d/1csJu4tYBaZvB9oDlcD5e0QL74RWPIZUV/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/phirom-kong-b8a2a129a",
+  twitter: "",
+  insta: "https://www.instagram.com/phirom_xx/",
+  facebook: "https://web.facebook.com/kong.phirom.09",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "Laravel",
+        image:"https://upload.wikimedia.org/wikipedia/commons/9/9a/Laravel.svg"
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Mobile  ",
+    skills: [
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "",
+    school: "Cambodia Academy of Digital Technology",
+    date: "2022 - Present",
+    grade: "8.71 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science specialized in Software Engineering at Cambodia Academy of Digital Technology. I am currently in my junior year with a cumulative GPA of 3.575. I have taken courses in Software Engineering, Algorithms, Object-Oriented Programming, Database Administration, Operating Systems, and Computer Architecture, among others.",
+    degree: "Bachelor of Computer Science",
+  },
+  {
+    id: 1,
+    img: "",
+    school: "Jay Prizter Academy",
+    date: "2008 - 2021",
+    grade: "88.0%",
+    desc: "I completed my diploma at Jay Pritzker Academy, where I received an education based on the American curriculum standards along with Khmer curriculum. I graduated with a B on my National Examination.",
+    degree: "Highschool Diploma",
+  },
+  {
+    id: 2,
+    img: "",
+    school: "National University of Singapore Science Camp",
+    date: "July 2028",
+    grade:"",
+    desc: "I attended the Science Camp at the National University of Singapore(NUS) back in 2018. This exchange program allows me to experience new cultures and work with other students from different background. This summer program taught me a lot about technologies and science.",
+    degree: "Summer Exchange Program",
+  },
+];
+
+export const projects = [
+    
+];
+
+export const TimeLineData = [
+
+];
