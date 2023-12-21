59c3866b71c7a8822c86cd2f89467139fca29f9d
exit <phirom.kong@student.cadt.edu.kh>
2023-12-21 19:00:49 +0700

    Contact update

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_dzdxue7', 'template_xntvlkq', form.current, 'SybVGsYS52j2TfLbi')
       .then((result) => {
         setOpen(true);
         form.current.reset();
